0296542212de7c5994878b41d090702b571c7c29
Ivana Mazzini <imazzini@est.frba.utn.edu.ar>
2018-11-18 20:40:15 -0300

    merge con GUI

--- Aplicacion Desktop/PalcoNet/Listado Estadistico/ClientesMuchasCompras.cs
+++ Aplicacion Desktop/PalcoNet/Listado Estadistico/ClientesMuchasCompras.cs
@@ -13,12 +13,12 @@ namespace PalcoNet.Listado_Estadistico
 {
     public partial class ClientesMuchasCompras : MiForm
     {
- /*       public ClientesMuchasCompras(List<CompraCliente> comprasClientes, MiForm formAnterior) : base(formAnterior)
-        {
+    /*   public ClientesMuchasCompras(List<CompraCliente> comprasClientes, MiForm formAnterior) : base(formAnterior)
+        { 
             InitializeComponent();
             var bindingList = new BindingList<CompraCliente>(comprasClientes);
             var source = new BindingSource(bindingList, null);
-            clientesComprasGrid.DataSource = source;*/
+            clientesComprasGrid.DataSource = source;
         }
 */
         private void button2_Click(object sender, EventArgs e)
