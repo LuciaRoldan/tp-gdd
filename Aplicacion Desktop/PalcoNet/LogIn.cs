43ae9459119019c7cf45a038f8ab12a33ec9ed0d
Ivana Mazzini <imazzini@est.frba.utn.edu.ar>
2018-11-20 00:28:51 -0300

    logica login

--- Aplicacion Desktop/PalcoNet/LogIn.cs
+++ Aplicacion Desktop/PalcoNet/LogIn.cs
@@ -9,15 +9,24 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Security.Cryptography;
+using PalcoNet.Dominio;
 
 namespace PalcoNet
 {
     public partial class LogIn : Form
     {
-        public static String usuario { get; set; }
+        
         Sesion sesion;
+        Usuario usuario;
+
+        public Usuario Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
         public LogIn()
         {
+            this.Usuario = usuario;
             InitializeComponent();
         }
 
@@ -28,6 +37,60 @@ namespace PalcoNet
 
         private void buttonAceptar_Click(object sender, EventArgs e)
         {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Servidor servidor = Servidor.getInstance();
+            StringBuilder Sb = new StringBuilder();
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(textBox2.Text.ToString()));
+
+                foreach (Byte b in result)
+                    Sb.Append(b.ToString("x2"));
+
+                Console.WriteLine("EL HASH ES:" + Sb);
+            }
+            try
+            {
+
+                servidor.realizarQuery("EXEC verificarLogin_sp '" + textBox1.Text.Trim() + "', '" + Sb.ToString() + "'");
+                Sesion s = Sesion.getInstance();
+                s.usuario = textBox1.Text.Trim();
+                List<String> roles = new List<String>();
+
+                String query = "SELECT r.nombre FROM UsuarioXRol ur JOIN Usuarios u ON (u.id_usuario = ur.id_usuario) JOIN Roles r ON (r.id_rol = ur.id_rol) WHERE username like ' " + s.usuario + "'";
+
+                SqlDataReader reader = servidor.query(query);
+
+                while (reader.Read())
+                {
+                    String rol;
+                    rol = reader["nombre"].ToString();
+                    roles.Add(rol);
+                }
+                reader.Close();
+
+                if (roles.Count() > 1)
+                {
+                      new SeleccionarRol().Show();
+                  }
+                  else
+                {
+                      new SeleccionarFuncionalidad().Show();
+                }
+            }
+            catch (SqlException ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Aplicacion Desktop/PalcoNet/Registro de Usuario/RegistroDeUsuario1.cs
+++ Aplicacion Desktop/PalcoNet/Registro de Usuario/RegistroDeUsuario1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 using System.Data.SqlClient;
 using PalcoNet.Dominio;
 
@@ -44,7 +45,19 @@ namespace PalcoNet.Registro_de_Usuario
                 {
                     Cliente cliente = new Cliente();
                     cliente.NombreUsuario = textBox1.Text;
-                    cliente.Contrasenia = textBox2.Text;
+                    
+                    StringBuilder Sb = new StringBuilder();
+                    using (SHA256 hash = SHA256Managed.Create())
+                    {
+                        Encoding enc = Encoding.UTF8;
+                        Byte[] result = hash.ComputeHash(enc.GetBytes(textBox2.Text.ToString()));
+
+                        foreach (Byte b in result)
+                            Sb.Append(b.ToString("x2"));
+
+                        Console.WriteLine("EL HASH ES:" + Sb.ToString());
+                    }
+                    cliente.Contrasenia = Sb.ToString();
                     new RegistroDeCliente(cliente,  this).Show();
                     this.Hide();
                 }
