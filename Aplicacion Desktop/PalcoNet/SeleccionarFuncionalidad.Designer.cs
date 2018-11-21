321101180a8fca6246ffe4a4b9cf03706fc2fabe
Ivana Mazzini <imazzini@est.frba.utn.edu.ar>
2018-11-21 02:11:36 -0300

    Agrega cada pantallita depende la funcionalidad que elija

--- Aplicacion Desktop/PalcoNet/SeleccionarFuncionalidad.Designer.cs
+++ Aplicacion Desktop/PalcoNet/SeleccionarFuncionalidad.Designer.cs
@@ -39,17 +39,19 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(16, 59);
+            this.comboBox1.Location = new System.Drawing.Point(24, 91);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(252, 21);
+            this.comboBox1.Size = new System.Drawing.Size(376, 28);
             this.comboBox1.TabIndex = 83;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(193, 127);
+            this.button2.Location = new System.Drawing.Point(290, 195);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 82;
             this.button2.Text = "Logout";
             this.button2.UseVisualStyleBackColor = true;
@@ -57,9 +59,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(16, 127);
+            this.button1.Location = new System.Drawing.Point(24, 195);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 81;
             this.button1.Text = "Cerrar";
             this.button1.UseVisualStyleBackColor = true;
@@ -68,9 +71,10 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 43);
+            this.label4.Location = new System.Drawing.Point(20, 66);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(255, 13);
+            this.label4.Size = new System.Drawing.Size(374, 20);
             this.label4.TabIndex = 80;
             this.label4.Text = "Seleccione la funcionalidad a la que desea acceder.";
             // 
@@ -78,32 +82,36 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(18, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 24);
+            this.label2.Size = new System.Drawing.Size(159, 33);
             this.label2.TabIndex = 79;
             this.label2.Text = "Bienvenido";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(193, 86);
+            this.button3.Location = new System.Drawing.Point(288, 129);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(112, 35);
             this.button3.TabIndex = 84;
             this.button3.Text = "Ir";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // SeleccionarFuncionalidad
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 169);
+            this.ClientSize = new System.Drawing.Size(426, 260);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "SeleccionarFuncionalidad";
             this.Text = "Seleccionar Funcionalidad";
             this.ResumeLayout(false);
--- Aplicacion Desktop/PalcoNet/SeleccionarFuncionalidad.cs
+++ Aplicacion Desktop/PalcoNet/SeleccionarFuncionalidad.cs
@@ -8,6 +8,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PalcoNet.Abm_Rol;
+using PalcoNet.Abm_Cliente;
+using PalcoNet.Registro_de_Usuario;
+using PalcoNet.Abm_Empresa_Espectaculo;
+using PalcoNet.Abm_Grado;
+using PalcoNet.Generar_Publicacion;
+using PalcoNet.Editar_Publicacion;
+using PalcoNet.Comprar;
+using PalcoNet.Historial_Cliente;
+using PalcoNet.Canje_Puntos;
+using PalcoNet.Generar_Rendicion_Comisiones;
+using PalcoNet.Listado_Estadistico;
 
 namespace PalcoNet
 {
@@ -21,8 +33,7 @@ namespace PalcoNet
 
             while (reader.Read())
             {
-                Console.WriteLine("ACA ESCRIBE " + reader["nombre"].ToString());
-                comboBox1.Items.Add(reader["nombre"].ToString());
+              comboBox1.Items.Add(reader["nombre"].ToString());
             }
             reader.Close();
         }
@@ -41,6 +52,60 @@ namespace PalcoNet
         {
             new LogIn().Show();
             this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {           
+
+            try
+            {
+                switch (comboBox1.Text.ToString())
+                {
+                    case "ABM de rol":
+                        new AbmRol(this).Show();
+                        break;
+                    case "Registro de usuario":
+                        new RegistroDeUsuario1(this).Show();
+                        break;
+                    case "ABM de cliente":
+                        new BusquedaCli(this).Show(); //ver si depende del rol 
+                        break;
+                    case "ABM de empresa de espectaculos":
+                        new BusquedaEmp(this).Show();
+                        break;
+                    case "ABM de categoria":
+                        //falta pantallita de abm categorias
+                        break;
+                    case "ABM grado de publicacion":
+                        // new GradoDeLaPublicacion(this).Show(); tiene que ser con empresa
+                        break;
+                    case " Generar publicacion":
+                        new CrearPublicacion().Show();
+                        break;
+                    case "Editar publicacion":
+                        new Editar_publicacion().Show();
+                        break;
+                    case "Comprar":
+                        //new BuscarP().Show(); es para cliente
+                        break;
+                    case "Historial del cliente":
+                        new Historial().Show();
+                        break;
+                    case "Canje y administracion de puntos":
+                        //new CanjePuntos().Show(); es para cliente
+                        break;
+                    case "Generar pago de comisiones":
+                        new Rendicion().Show();
+                        break;
+                    case "Listado estadistico":
+                        new Listados().Show();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
