c6ff79b884e6ff2eb367d1d4947fac3b5e754a1f
matigiorda <matiasgiorda27@gmail.com>
2018-12-05 03:41:33 -0300

    getFuncionalidadesDeRol_sp

---
+++ Procedures/getFuncionalidadesDeRol_sp.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE getFuncionalidadesDeRol_sp
+@nombre_rol VARCHAR(50)
+AS
+BEGIN
+	SELECT DISTINCT f.nombre FROM Funcionalidades f
+	JOIN Roles r ON (r.nombre = @nombre_rol)
+	JOIN FuncionalidadXRol fxr ON(fxr.id_rol= r.id_rol)
+END
