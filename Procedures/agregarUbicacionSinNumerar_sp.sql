e22e13cf1e0a2f6701bc58a0534d05fa9083f49b
matigiorda <matiasgiorda27@gmail.com>
2018-12-09 02:25:08 -0300

    Procedure generar espectáculo y ubicaciones, fixes en agregar ubicaciones numeradas y sin numerar

--- Procedures/agregarUbicacionSinNumerar_sp.sql
+++ Procedures/agregarUbicacionSinNumerar_sp.sql
@@ -1,5 +1,5 @@
 CREATE PROCEDURE agregarUbicacionSinNumerar_sp(
-@id_publicacion INT,
+@id_espectaculo INT,
 @tipo_ubicacion NVARCHAR(20),
 @cantidad INT,
 @precio NUMERIC(18)
@@ -7,12 +7,19 @@ CREATE PROCEDURE agregarUbicacionSinNumerar_sp(
 AS
 BEGIN
 	DECLARE @contador INT = 0
+	DECLARE @id_ubicacion INT
 
 	WHILE(@contador < @cantidad)
 	BEGIN
 
-		INSERT INTO Ubicaciones(fila, asiento, sin_numerar, precio)
-		VALUES(@tipo_ubicacion, NULL, NULL, 1, @precio)
+		INSERT INTO Ubicaciones(fila, asiento, sin_numerar, precio, tipo_ubicacion)
+		VALUES(NULL, NULL, 1, @precio, @tipo_ubicacion)
+
+		SET @id_ubicacion = SCOPE_IDENTITY()
+
+		INSERT INTO UbicacionXEspectaculo(id_espectaculo, id_ubicacion, id_compra)
+		VALUES (@id_espectaculo, @id_ubicacion, NULL)
+
 		SET @contador +=1
 
 	END
