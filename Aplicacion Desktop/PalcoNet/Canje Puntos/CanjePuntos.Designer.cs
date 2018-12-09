e2f6e7c6c7db07049bc025016331ff899c6cf685
Ivana Mazzini <imazzini@est.frba.utn.edu.ar>
2018-12-09 20:41:38 -0300

    Canje de puntos

--- Aplicacion Desktop/PalcoNet/Canje Puntos/CanjePuntos.Designer.cs
+++ Aplicacion Desktop/PalcoNet/Canje Puntos/CanjePuntos.Designer.cs
@@ -43,36 +43,40 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(18, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(148, 24);
+            this.label2.Size = new System.Drawing.Size(226, 33);
             this.label2.TabIndex = 108;
             this.label2.Text = "Canje de puntos";
             // 
             // textBoxPuntos
             // 
-            this.textBoxPuntos.Location = new System.Drawing.Point(102, 47);
+            this.textBoxPuntos.Location = new System.Drawing.Point(153, 72);
+            this.textBoxPuntos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxPuntos.Name = "textBoxPuntos";
             this.textBoxPuntos.ReadOnly = true;
-            this.textBoxPuntos.Size = new System.Drawing.Size(145, 20);
+            this.textBoxPuntos.Size = new System.Drawing.Size(216, 26);
             this.textBoxPuntos.TabIndex = 109;
             this.textBoxPuntos.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 50);
+            this.label1.Location = new System.Drawing.Point(20, 77);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.Size = new System.Drawing.Size(124, 20);
             this.label1.TabIndex = 110;
             this.label1.Text = "Sus puntos son:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 79);
+            this.label3.Location = new System.Drawing.Point(20, 122);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.Size = new System.Drawing.Size(95, 20);
             this.label3.TabIndex = 111;
             this.label3.Text = "Canjear por:";
             // 
@@ -80,17 +84,19 @@
             // 
             this.checkedListBoxPremios.CheckOnClick = true;
             this.checkedListBoxPremios.FormattingEnabled = true;
-            this.checkedListBoxPremios.Location = new System.Drawing.Point(16, 96);
+            this.checkedListBoxPremios.Location = new System.Drawing.Point(24, 148);
+            this.checkedListBoxPremios.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkedListBoxPremios.Name = "checkedListBoxPremios";
-            this.checkedListBoxPremios.Size = new System.Drawing.Size(231, 109);
+            this.checkedListBoxPremios.Size = new System.Drawing.Size(344, 151);
             this.checkedListBoxPremios.TabIndex = 112;
             this.checkedListBoxPremios.SelectedIndexChanged += new System.EventHandler(this.checkedListBoxPremios_SelectedIndexChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(16, 252);
+            this.button2.Location = new System.Drawing.Point(24, 388);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 35);
             this.button2.TabIndex = 113;
             this.button2.Text = "Inicio";
             this.button2.UseVisualStyleBackColor = true;
@@ -98,9 +104,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(172, 252);
+            this.button1.Location = new System.Drawing.Point(258, 388);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 35);
             this.button1.TabIndex = 114;
             this.button1.Text = "Canjear";
             this.button1.UseVisualStyleBackColor = true;
@@ -109,25 +116,28 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 223);
+            this.label4.Location = new System.Drawing.Point(20, 343);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.Size = new System.Drawing.Size(48, 20);
             this.label4.TabIndex = 116;
             this.label4.Text = "Total:";
             // 
             // textBoxTotal
             // 
-            this.textBoxTotal.Location = new System.Drawing.Point(53, 220);
+            this.textBoxTotal.Location = new System.Drawing.Point(80, 338);
+            this.textBoxTotal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxTotal.Name = "textBoxTotal";
             this.textBoxTotal.ReadOnly = true;
-            this.textBoxTotal.Size = new System.Drawing.Size(194, 20);
+            this.textBoxTotal.Size = new System.Drawing.Size(289, 26);
             this.textBoxTotal.TabIndex = 115;
+            this.textBoxTotal.TextChanged += new System.EventHandler(this.textBoxTotal_TextChanged);
             // 
             // CanjePuntos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(259, 287);
+            this.ClientSize = new System.Drawing.Size(388, 442);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBoxTotal);
             this.Controls.Add(this.button1);
@@ -137,6 +147,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxPuntos);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "CanjePuntos";
             this.Text = "Canje de puntos";
             this.ResumeLayout(false);
--- Aplicacion Desktop/PalcoNet/Canje Puntos/CanjePuntos.cs
+++ Aplicacion Desktop/PalcoNet/Canje Puntos/CanjePuntos.cs
@@ -14,61 +14,75 @@ namespace PalcoNet.Canje_Puntos
 {
     public partial class CanjePuntos : MiForm
     {
-        Servidor servidor = Servidor.getInstance();
+        Cliente cliente = new Cliente();
         Sesion sesion = Sesion.getInstance();
-        Cliente cliente;
+        Servidor servidor = Servidor.getInstance();
         int puntosOriginales;
         int puntosAcumulados = 0;
         List<Premio> premios = new List<Premio>();
+        Premio premio = new Premio();
+
+        public Cliente Cliente
+        {
+            get { return cliente; }
+            set { cliente = value; }
+        }
+
+        internal List<Premio> Premios
+        {
+            get { return premios; }
+            set { premios = value; }
+        }
+
+        public int PuntosOriginales
+        {
+            get { return puntosOriginales; }
+            set { puntosOriginales = value; }
+        }
+
+        public int PuntosAcumulados
+        {
+            get { return puntosAcumulados; }
+            set { puntosAcumulados = value; }
+        }
 
         public CanjePuntos(MiForm anterior) : base(anterior)
         {
-            Cliente cliente = new Cliente();
-            String username = sesion.usuario.NombreUsuario;
-            String descripcion;
-            int puntos;
-           
-            Console.WriteLine("LLEGA ACA CON ROL: " + sesion.rol.Nombre);
-
-                     if (sesion.rol.Nombre == "Cliente") {
-                         
-                          SqlDataReader reader = servidor.query("EXEC dbo.getPuntos_sp '" + username + "'");
-                          Console.WriteLine("ENTONCES ENTRA ACA CON USER: " + username);
-                          
-                         while (reader.Read())
-                          {
-                              Console.WriteLine(reader["cantidad_puntos"]);
-                              int guarda_puntos = Convert.ToInt32(reader["cantidad_puntos"]);
-                             
-                          }
-                          
-                          reader.Close();
-
-                           reader = servidor.query("EXEC dbo.getPremios_sp");
-
-                          while (reader.Read())
-                          {
-                              Console.WriteLine("LLEGA ACA TAMBIEN");
-                              Premio premio = new Premio();
-                              premio.Descripcion = reader["descripcion"].ToString();
-                              premio.CantidadDePuntos = Convert.ToInt32(reader["puntos"]);
-                              
-                        //      checkedListBoxPremios.Items.Add("descripcion" + " (" + "puntos" + " puntos)");
-            
-                                                           
-                          }
-                          reader.Close();
-                          Console.WriteLine("TERMINA LOS DOS WHILE");
-                        //  checkedListBoxPremios.Items.Add(premio.Descripcion + " (" + premio.CantidadDePuntos + " puntos)");
-
-                          //Aca hay que traer una lista de todos los premios de la base y guardarlos en la lista premios
-
-                      } else {
-                          MessageBox.Show("Se encuentra loggeado como " + sesion.rol.Nombre + " por lo cual no podrá utilizar esta funcionalidad.", "Advertencia", MessageBoxButtons.OK);
-                        button1.Enabled = false;
-      
-                      }
             InitializeComponent();
+            if (Sesion.getInstance().rol.Nombre == "Cliente")
+            {
+                this.Cliente.NombreUsuario = sesion.usuario.NombreUsuario;
+              //  this.PuntosOriginales = this.Cliente.Puntos;
+              //  this.textBoxPuntos.Text = this.Cliente.Puntos.ToString();
+
+                //Aca hay que traer una lista de todos los premios de la base y guardarlos en la lista premios
+
+                SqlDataReader reader = servidor.query("EXEC dbo.getPuntos_sp '" + sesion.usuario.NombreUsuario + "'");
+                while (reader.Read())
+                {
+                    puntosOriginales = Convert.ToInt32(reader["cantidad_puntos"]);
+                    textBoxPuntos.Text = puntosOriginales.ToString();
+
+                }
+                reader.Close();
+
+                reader = servidor.query("EXEC dbo.getPremios_sp");
+                while (reader.Read())
+                {
+                    premio.Descripcion = reader["descripcion"].ToString();
+                    premio.CantidadDePuntos = Convert.ToInt16(reader["puntos"]);
+                    this.Premios.Add(premio);
+                    checkedListBoxPremios.Items.Add(premio.Descripcion + " (" + premio.CantidadDePuntos + " puntos)");
+
+                }
+                reader.Close();
+            }
+            else
+            {
+                MessageBox.Show("Se encuentra loggeado como " + Sesion.getInstance().rol.Nombre + " por lo cual no podrá utilizar esta funcionalidad.", "Advertencia", MessageBoxButtons.OK);
+                button1.Enabled = false;
+            }
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -80,46 +94,60 @@ namespace PalcoNet.Canje_Puntos
         {
             //Aca hay que hacer que se cambien los puntos
             //Capaz estaria bueno que salga un cartelito de que salio todo bien
-            
-             cliente.Puntos = puntosOriginales - puntosAcumulados;
-            List<Premio> premiosSeleccionados = new List<Premio>();
-            foreach (int index in this.checkedListBoxPremios.SelectedIndices) {
-                premiosSeleccionados.Add(this.premios[index]);
+            if (this.puntosOriginales >= this.PuntosAcumulados)
+            {
+                servidor.realizarQuery("EXEC borrarPuntos_sp '" + this.PuntosAcumulados + "', '" + sesion.usuario.NombreUsuario + "'");
+                this.Cliente.Puntos = this.PuntosOriginales - this.PuntosAcumulados;
+                List<Premio> premiosSeleccionados = new List<Premio>();
+                foreach (int index in this.checkedListBoxPremios.SelectedIndices)
+                {
+                    premiosSeleccionados.Add(this.Premios[index]);
+                }
+                MessageBox.Show("El canje se realizó de forma exitosa.", "Premios", MessageBoxButtons.OK);
+
+                this.PuntosOriginales = this.PuntosOriginales - this.PuntosAcumulados;
+                foreach (int index in this.checkedListBoxPremios.SelectedIndices)
+                {
+                    checkedListBoxPremios.SetItemChecked(this.checkedListBoxPremios.SelectedIndex, false);
+                    this.PuntosAcumulados = this.PuntosAcumulados - this.Premios[this.checkedListBoxPremios.SelectedIndex].CantidadDePuntos;
+                }
+                this.textBoxPuntos.Text = this.PuntosOriginales.ToString();
+                this.textBoxTotal.Text = this.PuntosAcumulados.ToString();
             }
+            else
+            {
+                MessageBox.Show("Su monto actual de puntos no le permite canjear el obsequio, prube en otro momento.", "Premios", MessageBoxButtons.OK);
+            }
+            
 
             //Aca hay que actualizar los puntos del cliente y persistir los nuevos premios adquiridos
             //Como los puntos tienen un vencimiento hay que usar primero los puntos mas viejos y despues los mas nuevos
 
-            MessageBox.Show("El canje se realizó de forma exitosa.", "Premios", MessageBoxButtons.OK);
-
-            puntosOriginales = puntosOriginales - puntosAcumulados;
-            foreach (int index in this.checkedListBoxPremios.SelectedIndices){
-                checkedListBoxPremios.SetItemChecked(this.checkedListBoxPremios.SelectedIndex, false);
-                puntosAcumulados = puntosAcumulados - premios[this.checkedListBoxPremios.SelectedIndex].CantidadDePuntos;
-            }
-            this.textBoxPuntos.Text = puntosOriginales.ToString();
-            this.textBoxTotal.Text = puntosAcumulados.ToString();
 
             //Cambiar los puntos en el objeto cliente de la sesion
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            this.textBoxPuntos.Text = puntosAcumulados.ToString();
+
         }
 
         private void checkedListBoxPremios_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (this.checkedListBoxPremios.GetItemCheckState(this.checkedListBoxPremios.SelectedIndex) == CheckState.Checked){
-                if (puntosAcumulados + this.premios[this.checkedListBoxPremios.SelectedIndex].CantidadDePuntos <= puntosOriginales) {
-                    puntosAcumulados = puntosAcumulados + this.premios[this.checkedListBoxPremios.SelectedIndex].CantidadDePuntos;
-                } else {
-                    checkedListBoxPremios.SetItemChecked(this.checkedListBoxPremios.SelectedIndex, false);
-                }
-            } else {
-               puntosAcumulados = puntosAcumulados - this.premios[this.checkedListBoxPremios.SelectedIndex].CantidadDePuntos;
+            if (this.checkedListBoxPremios.GetItemCheckState(this.checkedListBoxPremios.SelectedIndex) == CheckState.Checked)
+            {
+                 this.PuntosAcumulados = this.PuntosAcumulados + this.Premios[this.checkedListBoxPremios.SelectedIndex].CantidadDePuntos;
             }
-            this.textBoxTotal.Text = puntosAcumulados.ToString();
+            else
+            {
+                this.PuntosAcumulados = this.PuntosAcumulados - this.Premios[this.checkedListBoxPremios.SelectedIndex].CantidadDePuntos;
+            }
+            this.textBoxTotal.Text = this.PuntosAcumulados.ToString();
+        }
+
+        private void textBoxTotal_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
