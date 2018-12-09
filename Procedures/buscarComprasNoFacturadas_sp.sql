940cefba8e9a94c95fd39a2c57877a9ec7459808
Lucia <lucia_roldan@outlook.com>
2018-12-09 13:45:11 -0300

    Saco pruebas que quedareon en la query

--- Procedures/buscarComprasNoFacturadas_sp.sql
+++ Procedures/buscarComprasNoFacturadas_sp.sql
@@ -7,16 +7,3 @@ create procedure buscarComprasNoFacturadas_sp (@razonSocial varchar(255)) as beg
 	join Empresas e on e.id_empresa = p.id_empresa and e.razon_social = @razonSocial
 	where id_factura is null 
 end
-
-select e.razon_social, .id_compra, descripcion, coalesce(comision, 0) comision, coalesce(importe, 0) importe from Compras c 
-	join UbicacionXEspectaculo u on c.id_compra = u.id_compra
-	join Espectaculos es on es.id_espectaculo = u.id_espectaculo
-	join Publicaciones p on p.id_publicacion = es.id_publicacion
-	join Empresas e on e.id_empresa = p.id_empresa
-	where id_factura is null 
-
-select * from Compras
-select * from UbicacionXEspectaculo
-update Compras set id_factura = null where id_factura = 129109
-
-EXEC dbo.buscarComprasNoFacturadas_sp 'Razon Social Nº:8'
