76b1032f0106582f512d6fdabf94518e9653b5d8
rorro <rociochipian@gmail.com>
2018-12-20 21:25:28 -0300

    buscar publi

--- ENTREGA/data/script_creación_inicial.sql
+++ ENTREGA/data/script_creación_inicial.sql
@@ -1266,35 +1266,13 @@ end
 GO
 
 
-/*
+
 -----buscarPublicacionesPorCriterio_sp-----
-CREATE PROCEDURE MATE_LAVADO.buscarPublicacionesPorCriterio_sp (@descripcion varchar(255), @categorias varchar(255), @desde datetime, @hasta datetime, @offset INT) as begin
+CREATE PROCEDURE MATE_LAVADO.buscarPublicacionesPorCriterio_sp (@descripcion varchar(255), @categorias varchar(255), @desde varchar(30), @hasta varchar(30), @offset INT) as begin
 	declare @query nvarchar(2000)
 	set @query = 
 	'select distinct p.descripcion descripcion, r.descripcion rubro, direccion, p.id_publicacion id, gr.nombre grado, p.id_grado_publicacion FROM MATE_LAVADO.Publicaciones p JOIN MATE_LAVADO.Espectaculos e on p.id_publicacion = e.id_publicacion 
 	LEFT JOIN MATE_LAVADO.Rubros r on r.id_rubro = p.id_rubro JOIN MATE_LAVADO.Grados_publicacion  gr on gr.id_grado_publicacion = p.id_grado_publicacion
-	where e.estado_espectaculo = ''Publicada'''
-
-	if ( @desde is not null and @hasta is not null) begin set @query = 
-		@query + ' and e.fecha_evento between ' + 
-		'CONVERT(DATETIME, ''' + (select convert(varchar, @desde, 25)) + ''', 121)' + ' and ' +  
-		'CONVERT(DATETIME, ''' + (select convert(varchar, @hasta, 25)) + ''', 121)' + ' ' end
-	if ( @descripcion is not null) begin set @query = @query + 'and p.descripcion like ''%' + @descripcion + '%''' end
-	if ( @categorias is not null) begin set @query = @query + 'and r.descripcion in (' + (@categorias) + ') ' end
-
-	set @query = @query + 'order by p.id_grado_publicacion ASC offset ' + (select convert(varchar, @offset)) + ' rows fetch next 10 rows only'
-	print @query
-	exec sp_executesql @query
-end
-GO
-*/
-
-
-create PROCEDURE MATE_LAVADO.buscarPublicacionesPorCriterio_sp (@descripcion varchar(255), @categorias varchar(255), @desde varchar(30), @hasta varchar(30), @offset INT) as begin
-	declare @query nvarchar(2000)
-	set @query = 
-	'select distinct p.descripcion descripcion, r.descripcion rubro, direccion, p.id_publicacion id, p.id_grado_publicacion FROM MATE_LAVADO.Publicaciones p JOIN MATE_LAVADO.Espectaculos e on p.id_publicacion = e.id_publicacion 
-	LEFT JOIN MATE_LAVADO.Rubros r on r.id_rubro = p.id_rubro
 	where e.estado_espectaculo = ''Publicada'''
 
 	if ( @desde is not null and @hasta is not null) begin set @query = 
@@ -1309,7 +1287,6 @@ create PROCEDURE MATE_LAVADO.buscarPublicacionesPorCriterio_sp (@descripcion var
 	exec sp_executesql @query
 end
 GO
-
 
 
 -----agregarEspectaculo-----
